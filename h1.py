7caf6324e749d9b34a1fb211a1213bea800bea4b
rekhaunnam123 <45434620+rekhaunnam123@users.noreply.github.com>
2018-12-14 00:15:39 +0530

    added h1.py
    this is a concept of slicing a string

---
+++ h1.py
@@ -0,0 +1,12 @@
+str='this is hackathon'
+print(str[0:4])
+#blow line takes default values for start and end values
+print(str[:])
+#now introducig step size
+print(str[0:14:2])
+#below code takes default values for start,end and stepsize
+print(str[::])
+#reverse string
+print(str[13:7:-1])
+#reverse string with default start and end values
+print(str[::-1])
